5cd54df0975891d14c3180672720b6359c1dad68
chocomilku <88021521+chocomilku@users.noreply.github.com>
2024-05-12 13:08:16 +0000

    feat: implemented answer subpage

--- client/src/pages/AppPage/index.tsx
+++ client/src/pages/AppPage/index.tsx
@@ -1,11 +1,18 @@
 import {
 	Badge,
+	Button,
 	Container,
 	Divider,
 	Flex,
+	FormControl,
+	FormHelperText,
+	FormLabel,
 	Heading,
 	Highlight,
+	Input,
 	Text,
+	Textarea,
+	VStack,
 } from "@chakra-ui/react";
 import GameDifficultySelectionSubPage from "./GameDifficultySelection";
 import Clock from "../../components/Clock";
@@ -30,37 +37,71 @@ const AppPage: React.FC = () => {
 					<Clock />
 				</Flex>
 				<Divider />
-				<Heading m="8" textAlign="center">
-					<Highlight
-						query="Topic:"
-						styles={{ p: "1", bg: "green.100", rounded: "6" }}>
-						Topic: Something
-					</Highlight>
-				</Heading>
-				<Divider />
-				<GameDifficultySelectionSubPage
-					onChange={handleCategoryChange}
-					selection={[
-						{
-							categoryId: 1,
-							categoryText: "easy",
-							categoryPoints: 2,
-							categoryColor: "blue",
-						},
-						{
-							categoryId: 2,
-							categoryText: "medium",
-							categoryPoints: 3,
-							categoryColor: "yellow",
-						},
-						{
-							categoryId: 3,
-							categoryText: "hard",
-							categoryPoints: 4,
-							categoryColor: "red",
-						},
-					]}
-				/>
+				{selectedCategory == null ? (
+					<>
+						<Heading m="8" textAlign="center">
+							<Highlight
+								query="Topic:"
+								styles={{ p: "1", bg: "green.100", rounded: "6" }}>
+								Topic: Something
+							</Highlight>
+						</Heading>
+						<Divider />
+						<GameDifficultySelectionSubPage
+							onChange={handleCategoryChange}
+							selection={[
+								{
+									categoryId: 1,
+									categoryText: "easy",
+									categoryPoints: 2,
+									categoryColor: "blue",
+								},
+								{
+									categoryId: 2,
+									categoryText: "medium",
+									categoryPoints: 3,
+									categoryColor: "yellow",
+								},
+								{
+									categoryId: 3,
+									categoryText: "hard",
+									categoryPoints: 4,
+									categoryColor: "purple",
+								},
+							]}
+						/>
+					</>
+				) : (
+					<>
+						<Heading m="8" textAlign="center">
+							<Highlight
+								query="Question:"
+								styles={{ p: "1", bg: "green.100", rounded: "6" }}>
+								Question: Something about monkeys
+							</Highlight>
+							<Badge
+								mx="2"
+								colorScheme={selectedCategory.categoryColor?.toString()}>
+								{selectedCategory.categoryText}
+							</Badge>
+						</Heading>
+						<Divider />
+						<VStack>
+							<FormControl my={2} px={{ base: "2", md: "8" }} isRequired>
+								<FormLabel>Name</FormLabel>
+								<Input />
+								<FormHelperText>Please type your last name.</FormHelperText>
+							</FormControl>
+							<FormControl my={2} px={{ base: "2", md: "8" }} isRequired>
+								<FormLabel>Answer</FormLabel>
+								<Textarea />
+							</FormControl>
+							<Button colorScheme="green" variant="outline" size="lg">
+								Submit
+							</Button>
+						</VStack>
+					</>
+				)}
 			</Container>
 		</>
 	);
