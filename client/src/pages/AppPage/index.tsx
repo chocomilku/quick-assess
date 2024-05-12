f77a8e94c13e2d180a3716c1554f938269e2cd83
chocomilku <88021521+chocomilku@users.noreply.github.com>
2024-05-12 02:39:48 +0000

    feat: added initial design

--- client/src/pages/AppPage/index.tsx
+++ client/src/pages/AppPage/index.tsx
@@ -1,11 +1,74 @@
-import { Container, Flex, Heading, Input } from "@chakra-ui/react";
+import {
+	Box,
+	Container,
+	Divider,
+	Flex,
+	Heading,
+	Highlight,
+	Text,
+} from "@chakra-ui/react";
 
 const AppPage: React.FC = () => {
 	return (
 		<>
-			<Container maxW="container.xl">
-				<Input my="8" />
-				<Flex></Flex>
+			<Container maxW="container.lg" bg="gray.50" p="8" minH="100vh">
+				<Text pb="4">STATUS: WHAR</Text>
+				<Divider />
+				<Heading m="8" textAlign="center">
+					<Highlight
+						query="Topic:"
+						styles={{ p: "1", bg: "green.100", rounded: "6" }}>
+						Topic: Something
+					</Highlight>
+				</Heading>
+				<Divider />
+				<Flex
+					gap="12"
+					wrap="wrap"
+					flexDirection="row"
+					justifyContent="center"
+					alignItems="center"
+					p="4">
+					<Box
+						maxW="sm"
+						border="2px"
+						borderColor="green.700"
+						p="3"
+						textAlign="center"
+						bg="green.100"
+						borderRadius="15"
+						cursor="pointer">
+						<Heading size="lg">CATEGORY</Heading>
+						<Divider my="2" />
+						<Text>+x points</Text>
+					</Box>
+					<Box
+						maxW="sm"
+						border="2px"
+						borderColor="green.700"
+						p="3"
+						textAlign="center"
+						bg="green.100"
+						borderRadius="15"
+						cursor="pointer">
+						<Heading size="lg">CATEGORY</Heading>
+						<Divider my="2" />
+						<Text>+x points</Text>
+					</Box>
+					<Box
+						maxW="sm"
+						border="2px"
+						borderColor="green.700"
+						p="3"
+						textAlign="center"
+						bg="green.100"
+						borderRadius="15"
+						cursor="pointer">
+						<Heading size="lg">CATEGORY</Heading>
+						<Divider my="2" />
+						<Text>+x points</Text>
+					</Box>
+				</Flex>
 			</Container>
 		</>
 	);
