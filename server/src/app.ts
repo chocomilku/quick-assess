3a917ecac2aac22c58a3d225318b422a0368fb5f
chocomilku <88021521+chocomilku@users.noreply.github.com>
2024-05-14 16:33:40 +0000

    preparing project to be deployed

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(
+	cors({
+		origin: ["http://localhost:4173", "http://localhost:5173"],
+		credentials: true,
+	})
+);
 app.use(logging);
 
 app.use("/api", routes);
